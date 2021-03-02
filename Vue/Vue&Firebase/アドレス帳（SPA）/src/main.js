b122eab965480109a9a958431030fb4da04706b8
Misora <50436693+iisora@users.noreply.github.com>
2021-03-02 17:56:21 +0900

    Update main.js
    configの内容を公開してしまっていたので、後々環境変数とかで補完したやつコミットし直す

--- Vue/Vue&Firebase/アドレス帳（SPA）/src/main.js
+++ Vue/Vue&Firebase/アドレス帳（SPA）/src/main.js
@@ -13,13 +13,13 @@ Vue.config.productionTip = false;
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAjaiUH3qp_TbQhaCGi0h_vcCf1tjQBwlo",
-  authDomain: "proven-gasket-297502.firebaseapp.com",
-  projectId: "proven-gasket-297502",
-  storageBucket: "proven-gasket-297502.appspot.com",
-  messagingSenderId: "783271402009",
-  appId: "1:783271402009:web:fe471311846cd5dbb76888",
-  measurementId: "G-LKT0Q1R01H",
+  apiKey: "aaaaaaaaaa",
+  authDomain: "aaaaaaaaa",
+  projectId: "aaaaaaaaaa",
+  storageBucket: "aaaaaaaaaa",
+  messagingSenderId: "aaaaaaaa",
+  appId: "aaaaaaaa",
+  measurementId: "aaaaaaaaaaaaaaaaa,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
